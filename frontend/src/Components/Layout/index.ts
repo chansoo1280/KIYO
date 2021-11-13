7724d59846360cb1906ba143ef7f6f8450bbf431
chansoo1280 <chansoo1280@naver.com>
2021-11-13 12:34:46 +0900

    Update index.ts

--- frontend/src/Components/Layout/index.ts
+++ frontend/src/Components/Layout/index.ts
@@ -1,3 +1,4 @@
+import { ForwardRefExoticComponent, RefAttributes } from "react"
 import { Default } from "./Default"
 export interface LayoutProps {
     children?: React.ReactNode
@@ -6,7 +7,7 @@ export enum LayoutCode {
     "Default",
 }
 const TheLayout: {
-    [key: number]: ({ children }: LayoutProps) => JSX.Element
+    [key: number]: (({ children }: LayoutProps) => JSX.Element) | ForwardRefExoticComponent<RefAttributes<HTMLDivElement>>
 } = {
     [LayoutCode.Default]: Default,
 }
